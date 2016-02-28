9e49b351648835f62d8a4f41c99f21b7330e2c5f
Zoran Robic <vajnar7@gmail.com>
2016-02-28 19:37:09 +0100

    Refactorisation of code

--- Software/Control/PositionControl/calc.c
+++ Software/Control/PositionControl/calc.c
@@ -7,100 +7,111 @@
 
 #include "moonstalker.h"
 
-t_telescope_coordinates cur_tel_pos;
-t_astronomical_coordinates cur_ast_coordinates;
-double cur_time;
+t_telescope_coordinates    cur_tel_pos;
+t_equatorial_coordinates   cur_eq_crds;
+double                     cur_time;
 
-int vtsk_astronomical_to_sferical(
-        t_astronomical_coordinates *in,
+/* 
+ * Converts equatorial to sferical coordinates 
+ * 
+ * in:   ra [0..24] 
+ *          right accesion, at the vernal equinox in hours
+ *       dec [+90..-90]
+ *          declination +90 on N pole, -90 S pole in degrees
+ *       latitude [+90..-90]
+ *          +90 on N pole, -90 on S pole in degrees
+ * 
+ * out:  d  delta->f(dec)      [rad]
+ *       fi fi   ->f(ra)       [rad]
+ *       t  theta->f(latitude) [rad]
+*/
+
+int vtsk_equatorial_to_sferical(
+        t_equatorial_coordinates   *in,
         t_sferical_coordinates     *out)
 {
-    double rec = in->rec;
+    double ra =  in->ra;
     double dec = in->dec;
-    
-    dec = 90 - dec;
-    
-    DEG_TO_RAD(out->d, dec);
-    DEG_TO_RAD(out->fi, (360 - (rec * 15.0)));
+    double tmp1;
+   
+    //delta
+    out->d = 90.0 - dec;
+    DEG_TO_RAD(out->d, out->d);
+
+    //fi
+    tmp1 = (ra * 15.0) + RA_OFFSET;
+    if(tmp1 > 360.0) tmp1 -= 360.0;
+    tmp1 = 360.0 - tmp1;
+    DEG_TO_RAD(out->fi, tmp1);
+
+    //theta 
     DEG_TO_RAD(out->t, in->latitude); 
 
+    //debug
+#if 0
+    VTSK_DEBUG("vtsk_astronomical_to_sferical:\n");
+    VTSK_DEBUG("\tra=%4.1lf, dec=%4.1lf latitude=%4.1lf\n", 
+          in->ra, in->dec, in->latitude);
+    VTSK_DEBUG("\tdelta=%4.1lf, fi=%4.1lf, theta=%4.1lf\n", 
+          out->d*VTSK_DEGREE, out->fi*VTSK_DEGREE, out->t*VTSK_DEGREE);
+#endif
+
     return(0);
 }
 
-int vtsk_astronomical_to_telescope(
-        t_astronomical_coordinates *in,
-        t_telescope_coordinates    *out)
+/* 
+ * Converts equatorial to telescope coordinates
+ * 
+ * in:   ra [0..24] 
+ *          right accesion, at the vernal equinox in hours
+ *       dec [+90..-90]
+ *          declination +90 on N pole, -90 S pole in degrees
+ *       latitude [+90..-90]
+ *          +90 on N pole, -90 on S pole in degrees
+ *
+ * out:  
+ *       azimuth
+ *       height
+ */
+int vtsk_equatorial_to_telescope(
+        t_equatorial_coordinates *in,
+        t_telescope_coordinates  *out)
 {
-    double tmp1, tmp2, tmp3;
-    double a, b, c;
-    double azimuth;
-    t_sferical_coordinates sfr;
+   double tmp1, tmp2, tmp3, tmp4;
+   double alpha, omega;
+   t_sferical_coordinates sfr;
 
-    vtsk_astronomical_to_sferical(in, &sfr);
+   vtsk_equatorial_to_sferical(in, &sfr);
 
-    /* alpha-> AZIMUTH */
-    tmp1 = a = cos(sfr.d)*cos(sfr.fi);
-    tmp2 = cos(sfr.t)*sin(sfr.d);
-    tmp3 = sin(sfr.t)*cos(sfr.d)*sin(sfr.fi);
-    tmp2 = b = tmp2 - tmp3;
-    printf("B=%lf\n", b);
+   // alpha-> azimuth
+   tmp1 = sin(sfr.d)*cos(sfr.fi);
+   tmp2 = cos(sfr.t)*cos(sfr.d);
+   tmp3 = sin(sfr.t)*sin(sfr.d)*sin(sfr.fi);
+   tmp4 = tmp2 - tmp3;
+   alpha = atan(tmp1/tmp2);
+   RAD_TO_DEG(out->azimuth, alpha);
+
+   // omega -> heigh
+   tmp1 = sin(sfr.t)*cos(sfr.d);
+   tmp2 = cos(sfr.t)*sin(sfr.d)*sin(sfr.fi);
+   omega = asin(tmp1 + tmp2); 
+   RAD_TO_DEG(out->height, omega);
+
+   //debug
 #if 0
-    if((0.0 == tmp2) && (tmp1 >= 0.0))
-        out->azimuth = PI/2.0;
-    else if((0.0 == tmp2) && (tmp1 < 0.0))
-        out->azimuth = -PI/2.0;
-    else if(0.0 != tmp2)
-#endif
-        out->azimuth = atan(tmp1/tmp2);
-
-    /* omega -> VISINA */
-    tmp1 = sin(sfr.t)*sin(sfr.d);
-    tmp2 = cos(sfr.t)*cos(sfr.d)*sin(sfr.fi);
-    out->height = asin(tmp1 + tmp2); 
-#if 0    
-    a = sin(sfr.d)*cos(sfr.fi);
-    b = cos(sfr.t)*cos(sfr.d) - sin(sfr.t)*sin(sfr.d)*sin(sfr.fi);
-    c = sin(sfr.t)*cos(sfr.d) + cos(sfr.t)*sin(sfr.d)*sin(sfr.fi); 
-#else
-    //a = sin(sfr.d)*cos(sfr.fi);
-    //b = sin(sfr.t)*cos(sfr.d) - cos(sfr.t)*sin(sfr.d)*sin(sfr.fi);
-    c = cos(sfr.t)*cos(sfr.d) + sin(sfr.t)*sin(sfr.d)*sin(sfr.fi); 
+   VTSK_DEBUG("vtsk_equatorial_to_telescope:\n");
+   VTSK_DEBUG("\tdelta=%4.1lf, fi=%4.1lf, theta=%4.1lf\n", 
+          sfr.d*VTSK_DEGREE, sfr.fi*VTSK_DEGREE, sfr.t*VTSK_DEGREE);
+   VTSK_DEBUG("\talpha=%4.1lf, omega=%4.1lf\n", 
+         alpha*VTSK_DEGREE, omega*VTSK_DEGREE);
 #endif
 
-    /* Robni Pogoji */
-    RAD_TO_DEG(out->azimuth, out->azimuth);
-    RAD_TO_DEG(out->height,  out->height);
-
-    azimuth = out->azimuth;
-    //printf("A=%lf B=%lf\n C=%lf\n", a, b, c);
-    printf("C=%lf\n", c);
-    printf("AZIMUTH=%lf\n", out->azimuth);
-
-    if((c < 0.0) && (b > 0.0))
-        azimuth = 180.0 - out->azimuth;
-    else if((c > 0.0) && (b < 0.0))
-        azimuth = 0.0 -out->azimuth;
-#if 0
-    else
-        azimuth = 360.0 - out->azimuth;
-
-    if((c == 0.0) && (out->azimuth == -90.0))
-    {
-        printf("Klobasa\n");
-        azimuth = 270.0;
-    }
-
-    //else
-    //else if((in->rec > 12) && (in->rec <= 24))
-    //    out->azimuth = 360 - out->azimuth;
-    
-    //if(out->azimuth >= 360.0) out->azimuth -= 360;
-#endif
-    out->azimuth = azimuth;
-
-    return(0);
+   return(0);
 }
 
+/* 
+ * Get current time 
+ */
 double vtsk_get_time()
 {
     time_t timer;
@@ -115,62 +126,84 @@ double vtsk_get_time()
     return seconds;
 }
 
+/* 
+ * Calibration
+ */
 int vtsk_calibration()
 {
-    printf(".....Calibration on 0, 0, 90\n");
+   char a;
 
-    /* Za primer Severnica!!!! TODO */
-    cur_ast_coordinates.rec =       0.0;
-    cur_ast_coordinates.dec =       0.0;
-    cur_ast_coordinates.latitude =  LAT;
-    vtsk_astronomical_to_telescope(&cur_ast_coordinates, &cur_tel_pos);
+   printf("Manually move telescope to POLARIS and hit ENTER\n");
+   scanf("%c", &a);
 
-    return(0);
+   printf("Telescope calibrated to POLARIS\n");
+   cur_eq_crds.ra =       RA_POLARIS;
+   cur_eq_crds.dec =      DEC_POLARIS;
+   cur_eq_crds.latitude = LATITUDE;
+   vtsk_equatorial_to_telescope(&cur_eq_crds, &cur_tel_pos);
+
+   return(0);
 }
 
-int vtsk_move(t_astronomical_coordinates *new_pos)
+/*
+ * Move
+ *
+ * in:   ra [0..24] 
+ *          right accesion, at the vernal equinox in hours
+ *       dec [+90..-90]
+ *          declination +90 on N pole, -90 S pole in degrees
+ *       latitude [+90..-90]
+ *          +90 on N pole, -90 on S pole in degrees
+ */
+
+int vtsk_move(t_equatorial_coordinates *new_pos)
 {
-    t_telescope_coordinates    tel;
-    double dif_az;
-    double dif_hi;
+    t_telescope_coordinates   tel;
+    double                    dif_az;
+    double                    dif_hi;
 
-    printf("Premik teleskopa na:\nRektascenzija = %lf\nDeklinacija = %lf\nLatitude = %lf",
-          new_pos->rec,
-          new_pos->dec,
-          new_pos->latitude);
+    vtsk_equatorial_to_telescope(new_pos, &tel);
 
-    vtsk_astronomical_to_telescope(new_pos, &tel);
-    printf("azimuth=%lf visina=%lf ", 
-            tel.azimuth, tel.height);
+    dif_az    = tel.azimuth - cur_tel_pos.azimuth;
+    dif_hi    = tel.height  - cur_tel_pos.height;
 
-    dif_az    = tel.azimuth  - cur_tel_pos.azimuth;
-    dif_hi    = tel.height   - cur_tel_pos.height;
-    printf("dif_az=%lf dif_heigh=%lf\n", dif_az, dif_hi);
-    cur_ast_coordinates.rec = new_pos->rec;
-    cur_ast_coordinates.dec = new_pos->dec;
-    cur_ast_coordinates.latitude = new_pos->latitude;
+    cur_eq_crds.ra = new_pos->ra;
+    cur_eq_crds.dec = new_pos->dec;
     cur_tel_pos.azimuth = tel.azimuth;
     cur_tel_pos.height = tel.height;
-    
+
+#if 1
+    //debug
+    VTSK_DEBUG("vtsk_move:\n");
+    VTSK_DEBUG("\tra=%lf, dec=%lf\n", 
+          new_pos->ra, new_pos->dec);
+    VTSK_DEBUG("\tdif_az=%lf, dif_hi=%lf\n",
+          dif_az, dif_hi);
+#endif
     return(0); 
 }
 
-#define VTSK_FOLLOW_TIME 1
-void vtsk_follow()
+/*
+ * Tracking
+ */
+void vtsk_track()
 {
-    double seconds = 0;
-    t_astronomical_coordinates ast;
+    t_equatorial_coordinates new_eq_crds;
+    double                   seconds = 0.0;
 
-    ast.rec = cur_ast_coordinates.rec;
-    ast.dec = cur_ast_coordinates.dec;
-    ast.latitude = cur_ast_coordinates.latitude;
+    printf("Tracking mode\n\n");
+
+    new_eq_crds.ra =       cur_eq_crds.ra;
+    new_eq_crds.dec =      cur_eq_crds.dec;
+    new_eq_crds.latitude = cur_eq_crds.latitude;
     do
     {
-        ast.rec = ast.rec -= SEC_TO_HOUR;
-        if(ast.rec < 0.0)   ast.rec = 24.0 + ast.rec;
-        if(ast.rec >= 24.0) ast.rec = ast.rec - 24.0;
-        printf("\t%d:: rec=%lf\n", (int)seconds, ast.rec);
-        vtsk_move(&ast);
+        new_eq_crds.ra = new_eq_crds.ra - SEC_TO_HOUR;
+#if 1
+        VTSK_DEBUG("Second: [%4.1lf]\n", seconds);
+        VTSK_DEBUG("RA=%lf\n", new_eq_crds.ra); 
+#endif
+        vtsk_move(&new_eq_crds);
         sleep(VTSK_FOLLOW_TIME);
         seconds++;
     } 
@@ -178,33 +211,50 @@ void vtsk_follow()
 }
 
 
-/* TODO: This is a reference function of low level priority */
+/*
+ * Draw
+ */
 void vtsk_draw()
 {
-    /* NOTE: priblizno to deluje le za cirkumpolarno polje */
-    initscr();          /* Start curses mode          */
-    printw("Hello World !!!");  /* Print Hello World          */
-    refresh();          /* Print it on to the real screen */
-    getch();            /* Wait for user input */
-    endwin();           /* End curses mode        */
+    initscr();                   //Start curses mode
+    printw("Hello World !!!");   //Print Hello World
+    refresh();                   //Print it on to the real screen
+    getch();                     //Wait for user input
+    endwin();                    //End curses modev
 }
 
+/*
+ * Main
+ */
 int main(int argc, char **argv)
 {
-    t_astronomical_coordinates ast;
+    t_equatorial_coordinates eq_crds;
 
+
+    eq_crds.ra =        RA;
+    eq_crds.dec =       DEC;
+    eq_crds.latitude =  LATITUDE;
+
+#if 0  
+    t_sferical_coordinates   sf_crds;
+    vtsk_equatorial_to_sferical(&eq_crds, &sf_crds);
+#endif
+#if 0
+    t_telescope_coordinates tel_crds;
+    vtsk_equatorial_to_telescope(&eq_crds, &tel_crds);
+#endif
+#if 1
     vtsk_calibration();
-
-    sleep(3);
-
-    ast.rec =       REC;
-    ast.dec =       DEC;
-    ast.latitude =  LAT;
-    vtsk_move(&ast);
-
-    sleep(1);
-    printf("Ajmo follow......\n\n");
-    vtsk_follow();
+#endif
+#if 1
+    vtsk_move(&eq_crds);
+#endif
+#if 1
+    char c;
+    printf("Hit ENTER to start tracking mode\n");
+    scanf("%c", &c);
+    vtsk_track();
+#endif
     return(0);
 }
 
--- Software/Control/PositionControl/include/moonstalker.h
+++ Software/Control/PositionControl/include/moonstalker.h
@@ -4,15 +4,32 @@
 #ifndef MOONSTALKER_H
 #define MOONSTALKER_H
 
-#define REC 23.0
-#define DEC 20.0
-#define LAT 45.0
+#define RA  15.0
+#define DEC 30.0
+
+#define LATITUDE 0.0 //equator
+
+/*
+ * Polaris coordinates
+ */
+#define RA_POLARIS 0.0
+#define DEC_POLARIS 90.0
+
+#define VTSK_WILDCARD 0.0
+
+
+#define VTSK_FOLLOW_TIME 1
+#define VTSK_DEBUG printf
+
+#define RA_OFFSET 9*15
 
 #define DEGREE 0.0174532925
 #define PI     3.141592654
 #define SEC_TO_HOUR (1.0 / 3600.0)
 #define DEG_TO_RAD(rad, deg) rad = deg * DEGREE;
 #define RAD_TO_DEG(deg, rad) deg = rad * (1 / DEGREE);
+
+#define VTSK_DEGREE (1/DEGREE)
 
 typedef struct
 {
@@ -29,17 +46,27 @@ typedef struct
 
 typedef struct
 {
-    double rec;
+    double ra;
     double dec;
     double latitude;
-} t_astronomical_coordinates;
+} t_equatorial_coordinates;
 
-int vtsk_astronomical_to_telescope(
-        t_astronomical_coordinates *in,
-        t_telescope_coordinates    *out);
+/* PROTOTYPES */
+int vtsk_equatorial_to_telescope(
+        t_equatorial_coordinates *in,
+        t_telescope_coordinates  *out);
 
-int vtsk_astronomical_to_sferical(
-        t_astronomical_coordinates *in,
-        t_sferical_coordinates     *out);
+int vtsk_equatorial_to_sferical(
+        t_equatorial_coordinates *in,
+        t_sferical_coordinates   *out);
 
+double vtsk_get_time();
+
+int vtsk_calibration();
+
+int vtsk_move(t_equatorial_coordinates *new_pos);
+
+void vtsk_track();
+
+void vtsk_draw();
 #endif
