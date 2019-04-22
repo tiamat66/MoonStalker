0b0b156d28169b68bf4779583ec770135f8c4c5c
Zoran Robic <vajnar7@gmail.com>
2019-04-22 10:10:36 +0200

    merged to master

--- Software/Control/Android/Moonstalker2.0/app/src/main/java/si/vajnartech/moonstalker/C.java
+++ Software/Control/Android/Moonstalker2.0/app/src/main/java/si/vajnartech/moonstalker/C.java
@@ -17,7 +17,7 @@ public final class C
   static final double DEF_LATITUDE = 46.45;
 
   // Named of paired BT device which acts like telescope
-  static final String SERVER_NAME = "Zoran Robič Tablca";
+  static final String SERVER_NAME = "MoonStalker";
 
   // Mechanical characteristics
   private static final double MOTOR_STEPS_NUM      = 200.0;
