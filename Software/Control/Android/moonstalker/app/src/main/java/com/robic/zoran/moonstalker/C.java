fa7adf42752cc9cdde85b8228eec2ec9dd0606ed
Zoran Robic <vajnar7@gmail.com>
2019-09-23 20:18:49 +0200

    deleted previous version of moonstalker

--- Software/Control/Android/moonstalker/app/src/main/java/com/robic/zoran/moonstalker/C.java
+++
@@ -1,7 +0,0 @@
-package com.robic.zoran.moonstalker;
-
-public class C
-{
-  // Named of paired BT device which acts like telescope
-  public static final String SERVER_NAME = "Zoran Robič Tablca";
-}
