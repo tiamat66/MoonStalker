587d1fd4e4abb617fb3eee9a2210e0d8dc0fd43e
vajnar7 <vajnar7@gmail.com>
2022-07-05 14:33:25 +0200

    Started version 2.0. Refactorization.

--- Software/Control/Android/Moonstalker/app/src/main/java/si/vajnartech/moonstalker/MainFragment.java
+++ Software/Control/Android/Moonstalker/app/src/main/java/si/vajnartech/moonstalker/MainFragment.java
@@ -4,17 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class MainFragment extends MyFragment
 {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
   {
-    View res = inflater.inflate(R.layout.content_main, container, false);
-    TextView tv = res.findViewById(R.id.msg_window);
-    tv.setVisibility(View.VISIBLE);
-    tv.setText(C.curMessage);
-    return res;
+    return inflater.inflate(R.layout.content_main, container, false);
   }
 }
