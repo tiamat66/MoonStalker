f6bd26aad841b91c466a6fa76c81dc71c942bc26
vajnar7 <vajnar7@gmail.com>
2026-08-04 11:09:50 +0200

    Updated and beautified ManualMoveFragment.

--- Software/Control/Android/Moonstalker/app/src/main/java/si/vajnartech/moonstalker/processor/Controller.java
+++ Software/Control/Android/Moonstalker/app/src/main/java/si/vajnartech/moonstalker/processor/Controller.java
@@ -5,7 +5,7 @@ import si.vajnartech.moonstalker.rest.RestBase;
 
 public abstract class Controller<R> extends RestBase<ObjController, R>
 {
-    public static String URL = "http://192.168.7.75:5000/";
+    public static String URL = "http://192.168.7.73:5000/";
     public static String PWD = "password123";
     public static String USR = "android";
 
